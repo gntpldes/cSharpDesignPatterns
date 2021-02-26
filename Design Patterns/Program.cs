53f0cf6811979441c751bb02313e3e2e78d3e4cd
gntpldes <41797111+gntpldes@users.noreply.github.com>
2021-02-26 08:34:22 -0500

    Add project files.

---
+++ Design Patterns/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console; 
+
+namespace Design_Patterns
+{
+    class Journal
+    {
+        private readonly List<string> entries = new List<string>();
+
+        private static int count = 0; 
+
+        public int AddEntry(string text)
+        {
+            entries.Add($"{++count}: {text}");
+            return count; 
+        }
+
+        public void RemoveEntry(int index)
+        {
+            entries.RemoveAt(index); 
+        }
+    }
+}
